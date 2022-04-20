c8cf7c97d28967baa8fb79e15a8860177b8d72a9
henry <cksgod16@gmail.com>
2022-04-20 13:18:11 +0900

    delete: all

--- jpa/README.md
+++
@@ -1,5 +0,0 @@
-<p align="center">
-    <samp>
-        <a href="https://spring.io/guides/gs/accessing-data-jpa/">spring jpa</a>
-    </samp>
-</p>
--- nonrest/src/main/java/payroll/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package payroll;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-}
