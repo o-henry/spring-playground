c6715c375e62620b4494f0978d7f19bcde5ebc9e
henry <cksgod16@gmail.com>
2022-05-03 14:48:40 +0900

    add: table and column to customer entity

--- spring-data-jpa/src/main/java/com/example/jpa/data/Customer.java
+++ spring-data-jpa/src/main/java/com/example/jpa/data/Customer.java
@@ -1,23 +1,30 @@
 package com.example.jpa.data;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 // JPA를 사용하기 위해서 Domain Entity를 정의해야 한다.
-// rdbms 와 연결하지 않았으므로 in-memory 에 저장된다
+// *(Table이나 column이 없는 상태) → rdbms 와 연결하지 않았으므로 in-memory 에 저장된다
 @Entity // This Tells Hibernate to make a table out of this class
+@Table(name = "customer")
 public class Customer {
 
   @Id // annotation을 통해 JPA가 이를 객체의 ID로 인식합니다.
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Integer id;
 
-  // default-value를 추가하고싶다면 builder pattern을 사용해야 한다.
+
+  @Column(name = "first_name", nullable = false)
   private String firstName;
+
+  @Column(name = "last_name", nullable = false)
   private String lastName;
 
+  @Column(name = "email", nullable = false)
   private String email;
 
   // JPA를 위해서 존재하는 생성자
@@ -27,6 +34,7 @@ public class Customer {
   ;
 
   // DB에 저장할 Customer 인스턴스를 생성하는 생성자
+  // default-value를 추가하고싶다면 builder pattern을 사용해야 한다.
   public Customer(String firstName, String lastName, String email) {
     this.firstName = firstName;
     this.lastName = lastName;
